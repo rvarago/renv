5cd8aad632073e8f25f6882d0a2a030c782d4e6d
Rafael Varago <varago.rafael@gmail.com>
2021-07-17 16:16:04 +0200

    Extend shell.nix Haskell template

--- templates/nix/haskell/shell.nix
+++ templates/nix/haskell/shell.nix
@@ -3,6 +3,14 @@
 with pkgs;
 
 let
-  ghc = haskellPackages.ghcWithPackages
-    (ps: with ps; [ conduit lens mtl polysemy servant ]);
+  ghc = haskellPackages.ghcWithPackages (ps:
+    with ps; [
+      conduit
+      lens
+      megaparsec
+      mtl
+      polysemy
+      servant
+      transformers
+    ]);
 in mkShell { buildInputs = [ ghc hlint ormolu ]; }
