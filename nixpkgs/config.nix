a6a3e0c33424c5728901aaca016e87cc8d926836
Rafael Varago <varago.rafael@gmail.com>
2021-07-18 10:32:26 +0200

    Drop hacky overrides for Fish foreign-env

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -1,8 +1,5 @@
 { pkgs }: {
 
-  # FIXME: https://github.com/nix-community/home-manager/issues/1702.
-  packageOverrides = pkgs: { fish-foreign-env = pkgs.fishPlugins.foreign-env; };
-
   allowUnfree = true;
 }
 
