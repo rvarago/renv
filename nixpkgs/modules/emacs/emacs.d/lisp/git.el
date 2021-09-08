4b6912a850e02774094b43a3f4dcd4049eb2775f
Rafael Varago <rafael.varago@gmail.com>
2021-09-08 21:45:40 +0200

    Remap magit/git-timemachine keybindins
    Instead of C-x g mapping to magit-status, we now
    have that C-x g opens a sub-groups where
    keybindins to some magit's (and also git-timemachine)
    are added.

--- nixpkgs/modules/emacs/emacs.d/lisp/git.el
+++ nixpkgs/modules/emacs/emacs.d/lisp/git.el
@@ -1,10 +1,20 @@
-;; ============================ Magit ============================
+;; ============================ Git integration ============================
 
 (use-package magit
-  :bind (("C-x g" . magit-status)))
+  :bind
+  ("C-x g s" . magit-status)
+  ("C-x g x" . magit-checkout)
+  ("C-x g c" . magit-commit)
+  ("C-x g p" . magit-push)
+  ("C-x g f" . magit-fetch)
+  ("C-x g u" . magit-pull)
+  ("C-x g e" . magit-ediff-resolve)
+  ("C-x g r" . magit-rebase)
+  ("C-x g i" . magit-rebase-interactive)
+  ("C-x g l" . magit-log))
 
 (use-package diff-hl
   :init (global-diff-hl-mode))
 
 (use-package git-timemachine
-  :bind (("C-x v t" . git-timemachine)))
+  :bind (("C-x g t" . git-timemachine)))
