6485701ef931c0c48f8944f00c23f2cf6fcd893e
Rafael Varago <rafael.varago@gmail.com>
2021-09-08 21:15:22 +0200

    Sort init.el loads and drop unnecessary ensure bound on diminish

--- nixpkgs/modules/emacs/emacs.d/init.el
+++ nixpkgs/modules/emacs/emacs.d/init.el
@@ -6,15 +6,15 @@
 (eval-when-compile
   (require 'use-package))
 
-(use-package diminish
-  :ensure t)
+(use-package diminish)
 
+;; TODO: Extract root from some constant (e.g. maybe with getenv).
 (load-file "~/.emacs.d/lisp/edit.el")
-(load-file "~/.emacs.d/lisp/ui.el")
-(load-file "~/.emacs.d/lisp/mini.el")
-(load-file "~/.emacs.d/lisp/project.el")
 (load-file "~/.emacs.d/lisp/git.el")
-(load-file "~/.emacs.d/lisp/nav.el")
-(load-file "~/.emacs.d/lisp/window.el")
-(load-file "~/.emacs.d/lisp/term.el")
 (load-file "~/.emacs.d/lisp/ide.el")
+(load-file "~/.emacs.d/lisp/mini.el")
+(load-file "~/.emacs.d/lisp/nav.el")
+(load-file "~/.emacs.d/lisp/project.el")
+(load-file "~/.emacs.d/lisp/term.el")
+(load-file "~/.emacs.d/lisp/ui.el")
+(load-file "~/.emacs.d/lisp/window.el")
