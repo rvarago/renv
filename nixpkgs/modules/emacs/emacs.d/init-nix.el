6c0b0f857abbe19c2ab0edef66bf9eb3083d049e
Rafael Varago <rafael.varago@gmail.com>
2021-09-20 17:50:50 +0200

    Delete old vanilla emacs module
    It has been replaced by doom emacs.

--- nixpkgs/modules/emacs/emacs.d/init-nix.el
+++
@@ -1,6 +0,0 @@
-;;; init-nix.el --- Base initialization for nix systems.
-;;; Commentary:
-;; Set up base where packages are expected to be installed via nix.
-
-(eval-when-compile
-  (require 'use-package))
