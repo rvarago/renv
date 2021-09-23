bb7f84d0c9a6ea9224d2f97fc161357630f500bf
Rafael Varago <rafael.varago@gmail.com>
2021-09-23 18:58:14 +0200

    Enable scheme (guile) and racket modes

--- nixpkgs/modules/emacs-doom/doom.d/init.el
+++ nixpkgs/modules/emacs-doom/doom.d/init.el
@@ -154,14 +154,14 @@
        ;;purescript        ; javascript, but functional
        (python +lsp +pyright +pyenv +poetry)            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
-       ;;racket            ; a DSL for DSLs
+       (racket +xs)            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
        rest              ; Emacs as a REST client
        ;;rst               ; ReST in peace
        ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        (rust +lsp)              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        (scala +lsp)             ; java, but good
-       ;;(scheme +guile)   ; a fully conniving family of lisps
+       (scheme +guile)   ; a fully conniving family of lisps
        (sh +lsp +fish)                ; she sells {ba,z,fi}sh shells on the C xor
        ;;sml
        ;;solidity          ; do you need a blockchain? No.
