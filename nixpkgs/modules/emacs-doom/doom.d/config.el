dfcccd9ed917eda3a61086e3917828580c862397
Rafael Varago <rafael.varago@gmail.com>
2021-10-06 17:26:27 +0200

    Increase company-idle-delay to make it less intrusive

--- nixpkgs/modules/emacs-doom/doom.d/config.el
+++ nixpkgs/modules/emacs-doom/doom.d/config.el
@@ -98,7 +98,7 @@
 ;; ================= COMPLETION =================
 
 (use-package! company
-  :config (setq company-idle-delay 0.2
+  :config (setq company-idle-delay 0.3
                 company-minimum-prefix-length 2
                 company-require-match nil)
   :bind (:map company-mode-map
