0673181958a1380f5ef9b67240b8e2b048c8e3f8
Rafael Varago <rafael.varago@gmail.com>
2022-07-03 09:56:18 +0200

    Rename alias `review` to `greview`
    To remind me that's about gerrit.

--- nixpkgs/modules/vcs/default.nix
+++ nixpkgs/modules/vcs/default.nix
@@ -65,7 +65,7 @@
         "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all";
       pf = "push --force-with-lease";
       ria = "rebase --interactive --autosquash";
-      review = "push HEAD:refs/for/master";
+      greview = "push HEAD:refs/for/master";
       st = "status";
       vdiff = "difftool --dir-diff --no-prompt";
     };
