fe56a1a73b031e7802b0bb8cf79d3f9d63c5ee0b
Rafael Varago <rafael.varago@gmail.com>
2025-02-25 16:39:16 +0100

    replace niv with flake + flatten modules

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "My Nix env";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs =
+    {
+      nixpkgs,
+      home-manager,
+      ...
+    }:
+    let
+      # "aarch64-linux" # 64-bit ARM Linux
+      # "x86_64-darwin" # 64-bit Intel macOS
+      # "aarch64-darwin" # 64-bit ARM macOS
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      settings = import ./settings.nix;
+    in
+    {
+      homeConfigurations = {
+        settings.user = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          extraSpecialArgs = { inherit settings; };
+
+          modules = [
+            ./nixpkgs/home.nix
+          ];
+
+        };
+      };
+    };
+}
--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -1,13 +1,15 @@
-{ config, pkgs, lib, ... }:
+{
+  pkgs,
+  lib,
+  ...
+}:
 
 let
   settings = import ./settings.nix;
-  user = ./users + "/${settings.user}.nix";
-in {
+in
+{
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
-
-  imports = [ (import user { inherit pkgs lib settings; }) ];
 
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
@@ -18,4 +20,92 @@ in {
   # the Home Manager release notes for a list of state version
   # changes in each release.
   home.stateVersion = "21.11";
+
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = settings.user;
+  home.homeDirectory = "${settings.user}";
+
+  home.sessionVariables = {
+    USER_FULL_NAME = settings.userFullName;
+    USER_EMAIL = settings.userEmail;
+  };
+
+  home.packages = with pkgs; [
+    # CLI tools.
+    bat
+    bc
+    eza
+    fd
+    fzf
+    graphviz
+    htop
+    imagemagick
+    jq
+    pandoc
+    procs
+    ripgrep
+    rlwrap
+    socat
+    tealdeer
+    tokei
+    tree
+    xclip
+    xdot
+    xsv
+    wget
+    #wkhtmltopdf
+
+    # Dev tools.
+    minicom
+    plantuml
+    protobuf
+    valgrind
+    wireshark
+    patchelf
+
+    # Cloud.
+    docker-compose
+    helm
+    kind
+    kubectl
+    kubectx
+    #minikube
+    hadolint
+    #terraform
+
+    # Writing.
+    aspell
+    #aspellDicts.de
+    aspellDicts.en
+    aspellDicts.en-computers
+    aspellDicts.en-science
+    aspellDicts.pt_BR
+    languagetool
+    #texlive.combined.scheme-small
+
+    # Fonts.
+    nerd-fonts.iosevka
+    nerd-fonts.jetbrains-mono
+    jetbrains-mono
+  ];
+
+  fonts.fontconfig.enable = true;
+
+  imports = [
+    ./modules/emacs
+    ./modules/neovim.nix
+
+    ./modules/tmux.nix
+    ./modules/kitty.nix
+    ./modules/fish.nix
+
+    (import ./modules/git.nix { inherit pkgs settings; })
+
+    ./modules/langs.nix
+
+    ./modules/linux.nix
+
+  ] ++ lib.optional (builtins.pathExists ./modules/ephemeral.nix) ./modules/ephemeral.nix;
+
 }
